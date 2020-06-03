2fb6d7ee18e76df456b9619445a52e9d2e5cab04
Kharanshu <dewankharanshu@gmail.com>
2020-06-04 02:17:14 +0530

    Updates

---
+++ precache-manifest.54e3753ec759de76f63b89531f7725a1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbff79e6402390b67bb37436ff86178c",
+    "url": "/Intern-project/index.html"
+  },
+  {
+    "revision": "aa78017a4753e3614caa",
+    "url": "/Intern-project/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "26899401349db2526749",
+    "url": "/Intern-project/static/css/main.15237ae5.chunk.css"
+  },
+  {
+    "revision": "aa78017a4753e3614caa",
+    "url": "/Intern-project/static/js/2.4f95627f.chunk.js"
+  },
+  {
+    "revision": "edbc79938c564134066dc08722bf82e3",
+    "url": "/Intern-project/static/js/2.4f95627f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26899401349db2526749",
+    "url": "/Intern-project/static/js/main.ba15066b.chunk.js"
+  },
+  {
+    "revision": "258a12f7ccf956ef9aa2",
+    "url": "/Intern-project/static/js/runtime-main.6f891322.js"
+  },
+  {
+    "revision": "8e47d7f592e3366311fbc67ed19f9065",
+    "url": "/Intern-project/static/media/calendar.8e47d7f5.svg"
+  },
+  {
+    "revision": "6213899184dd5f1ec0266c07d17884ed",
+    "url": "/Intern-project/static/media/competition.62138991.jpg"
+  },
+  {
+    "revision": "645338e7079ca85a6c5d6e5e1b65fc6b",
+    "url": "/Intern-project/static/media/d2clogo.645338e7.svg"
+  },
+  {
+    "revision": "eee0d24eeee686a46bee2947af5a7455",
+    "url": "/Intern-project/static/media/sportsquiz.eee0d24e.png"
+  }
+]);
